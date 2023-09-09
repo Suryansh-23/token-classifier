ea6093dc3829a79ae746bac44885072d1e3735ac
Suryansh Chauhan <suryanshchauhan2005@gmail.com>
2023-09-09 14:28:02 +0530

    preprocess changes

--- preprocess.py
+++ preprocess.py
@@ -19,8 +19,8 @@ def merge_dicts(d1, d2):
 
 # loop across all the files and merge them into one
 for file in files:
-    with open(f"./data/{file}", "r", encoding="utf-8") as fp:
-        content = fp.read().strip()
+    with open(f"./data/{file}", "r", encoding="utf-8") as jp:
+        content = jp.read().strip()
         obj = xtd.parse(content)["document"]
 
         if "#text" in obj.keys():
@@ -28,7 +28,49 @@ for file in files:
 
     main = merge_dicts(main, obj)  # merge the dictionaries
 
-# final file with all the data
+# # final file with all the data
+# with open(f"./json/main.json", "w", encoding="utf-8") as jp:
+#     json.dump(
+#         main,
+#         jp,
+#         indent=4,
+#     )
+
+json_files = os.listdir("./json")  # list of all the files in the json folder
+main = []
+
+
+def handle_data(data, k):
+    if data == None:
+        return
+    if type(data) == str:
+        main.append((data, k))
+    elif type(data) == dict:
+        if "#text" in data.keys():
+            main.append((data["#text"], k))
+    elif type(data) == list:
+        for j in data:
+            if type(j) == str:
+                main.append((j, k))
+            elif type(j) == dict:
+                if "#text" in j.keys():
+                    main.append((j["#text"], k))
+            else:
+                print("inlist", data, k)
+                print("err")
+    else:
+        print("nontype", data, k)
+        exit()
+
+
+for file in json_files:
+    with open(f"./json/{file}", encoding="utf-8") as jp:
+        obj = json.load(jp)
+
+    for k, v in obj.items():
+        for i in v:
+            handle_data(i, k)
+
 with open(f"./json/main.json", "w", encoding="utf-8") as jp:
     json.dump(
         main,
